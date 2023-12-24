e7f342b967a73e09badb8e0e2c06ca405389a419
Mathieu RIGARD <mathieu.rigard@protonmail.com>
2023-12-24 03:16:58 +0100

    WIP

--- cpp/unit_tests/puzzle_solver_unit_test.cpp
+++ cpp/unit_tests/puzzle_solver_unit_test.cpp
@@ -9,21 +9,34 @@
 #include "core/puzzle_manager.h"
 
 
+// Validate the puzzle pieces are in place
+TEST(PuzzleSolver, PuzzleValidation) {
+    auto puzzleManager = PuzzleManager();
+
+    puzzleManager.loadFromJsonFile(PUZZLE_VERY_EASY_DATA_FILE_PATH);
+}
+
+// Get all conflict between pieces
+TEST(PuzzleSolver, GetPuzzlePiecesConflicts) {
+    auto puzzleManager = PuzzleManager();
+
+    puzzleManager.loadFromJsonFile(PUZZLE_VERY_EASY_DATA_FILE_PATH);
+}
+
 // Puzzle solver basic algo
 TEST(PuzzleSolver, BasicAlgorithm) {
     auto puzzleManager = PuzzleManager();
 
     puzzleManager.loadFromJsonFile(PUZZLE_VERY_EASY_DATA_FILE_PATH);
-    puzzleManager.solve(PuzzleSolver::ALGORITHM_BASIC_BRUTE_FORCE);
-
+    // puzzleManager.solve(PuzzleSolver::ALGORITHM_BASIC_BRUTE_FORCE);
 }
 
-// Puzzle solver basic algo
+// Puzzle solver basic algo frame
 TEST(PuzzleSolver, FrameOnly) {
     auto puzzleManager = PuzzleManager();
 
     puzzleManager.loadFromJsonFile(PUZZLE_VERY_EASY_DATA_FILE_PATH);
-    puzzleManager.solve(PuzzleSolver::ALGORITHM_ONLY_FRAME);
+    // puzzleManager.solve(PuzzleSolver::ALGORITHM_ONLY_FRAME);
 }
 
 #endif  // UNIT_TEST_MODE
